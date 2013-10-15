9bf06b5da2b4cb804d00c6df80d0b871da4a1a56
neostark <patrice.boimond@gmail.com>
2013-10-15 17:39:26 +0200

    Database modified : - added primary/foreign key - changed names

--- sportBet/database_creation.sql
+++ sportBet/database_creation.sql
@@ -1,45 +1,49 @@
-CREATE TABLE APP.CompetitionTable (
-		id 				int,
-		competName 		VARCHAR(32),
-		competYear 		VARCHAR(4)
+CREATE TABLE APP.COMPETITIONS (
+		competID 		int NOT NULL PRIMARY KEY,
+		competCountry	VARCHAR(32) NOT NULL,
+		competName 		VARCHAR(32) NOT NULL,
+		competYear 		VARCHAR(4) NOT NULL
 	);
 
-CREATE TABLE APP.MatchTable (
-		competID		int,
-		id				int,
-		team1			VARCHAR(32),
-		team2			VARCHAR(32),
-		matchDate		timestamp
+CREATE TABLE APP.MATCHES (
+		gameID			int NOT NULL PRIMARY KEY,
+		team1			VARCHAR(32) NOT NULL,
+		team2			VARCHAR(32) NOT NULL,
+		matchDate		timestamp NOT NULL, 
+		competID		int NOT NULL REFERENCES APP.COMPETITIONS(competID)
 	);
 	
 CREATE TABLE APP.STATISTICS (
-		gameID			int, 
-		team			VARCHAR(32)
+		statID			int NOT NULL PRIMARY KEY,
+		team			VARCHAR(32) NOT NULL,
+		gameID			int NOT NULL REFERENCES APP.MATCHES(gameID)
 	);
 	
-CREATE TABLE APP.COMPOSITION (
-		gameID			int,
-		team			VARCHAR(32),
-		player1			VARCHAR(32),
-		player2			VARCHAR(32),
-		player3			VARCHAR(32),
-		player4			VARCHAR(32),
-		player5			VARCHAR(32),
-		player6			VARCHAR(32),
-		player7			VARCHAR(32),
-		player8			VARCHAR(32),
-		player9			VARCHAR(32),
-		player10		VARCHAR(32),
-		player11		VARCHAR(32)
+CREATE TABLE APP.COMPOSITIONS (
+		compoID			int NOT NULL PRIMARY KEY,
+		team			VARCHAR(32) NOT NULL,
+		player1			VARCHAR(32) NOT NULL,
+		player2			VARCHAR(32) NOT NULL,
+		player3			VARCHAR(32) NOT NULL,
+		player4			VARCHAR(32) NOT NULL,
+		player5			VARCHAR(32) NOT NULL,
+		player6			VARCHAR(32) NOT NULL,
+		player7			VARCHAR(32) NOT NULL,
+		player8			VARCHAR(32) NOT NULL,
+		player9			VARCHAR(32) NOT NULL,
+		player10		VARCHAR(32) NOT NULL,
+		player11		VARCHAR(32) NOT NULL,
+		gameID			int NOT NULL REFERENCES APP.MATCHES(gameID)
 	);
 
-CREATE TABLE APP.GAMEEVENT (
-		gameID			int,
-		team			VARCHAR(32),
-		event			VARCHAR(30),
-		minuteOfEvent	int, 
-		playerConcerned	VARCHAR(32),
-		playerEntering	VARCHAR(32)
+CREATE TABLE APP.GAMEEVENTS (
+		gameEventID		int NOT NULL PRIMARY KEY,
+		team			VARCHAR(32) NOT NULL,
+		event			VARCHAR(30) NOT NULL,
+		minuteOfEvent	int NOT NULL, 
+		playerConcerned	VARCHAR(32) NOT NULL,
+		playerEntering	VARCHAR(32) NOT NULL,
+		gameID			int NOT NULL REFERENCES APP.MATCHES(gameID)
 	);
 		
 		
